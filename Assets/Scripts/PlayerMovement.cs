0755a0e8fca29fad1bdb2725f23eb0cf925d610c
ehrenholgersson <112737007+ehrenholgersson@users.noreply.github.com>
2024-02-05 11:45:46 +1100

    Improved (hopefully) jump responsiveness by allowing leniency on input and ground check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour, IKillable
 {
+    [SerializeField] float _groundCheckLeniency;
+    [SerializeField] float _jumpInputTime;
+
     Rigidbody2D _rb;
     bool _grounded;
+    float _groundedTime;
+    float _jumpRequest = -10;
+    float _lastJump;
+
     ParticleSystem _particle;
     bool _doubleJump = false;
     Color _transperent = new Color(0, 0, 0, 0);
@@ -20,14 +27,27 @@ public class PlayerMovement : MonoBehaviour, IKillable
     // Update is called once per frame
     void Update()
     {
-        //player controls
-        if (_grounded)
+        // record time the jump key was hit so we can still respond if player is slightly early
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space)&& _rb.velocity.y < 10)
+            _jumpRequest = Time.time;
+        }
+
+        //player controls
+        if (_groundedTime < _groundCheckLeniency) // allow some leniency on ground check when jumping to help feel responsive
+        {
+            if (Mathf.Abs(_jumpRequest - Time.time) < _jumpInputTime && _rb.velocity.y < 10 && Time.time - _lastJump > (_groundCheckLeniency + 0.025f))
             {
-                _rb.velocity += new Vector2(0, 8 );
+                _rb.velocity += new Vector2(0, 8);
                 _rb.angularVelocity = (_rb.velocity.x + GameControl.Instance.WorldSpeed) * -10;
+                _jumpRequest = -10;
+                // this was allowing 2 "grounded" jumps if key was hit just before then during collision with terrain, need to make sure at least one physics tick + our input leniency have past since last jump to prevent this, also remove any groudcheck leniency
+                _groundedTime = 10;
+                _lastJump = Time.time;
             }
+        }
+        if (_grounded) // still need to use bool for left/right, otherwise we get a big movement boost on jumping with key held in
+        { 
             if (Input.GetKey("a")&&_rb.velocity.x>-10)
             {
                 _rb.velocity += new Vector2(-35, 0) * Time.deltaTime;
@@ -38,11 +58,12 @@ public class PlayerMovement : MonoBehaviour, IKillable
             }
         }
         else if (_doubleJump)
-            if (Input.GetKeyDown(KeyCode.Space) && _rb.velocity.y < 10)
+            if (Mathf.Abs(_jumpRequest - Time.time) < _jumpInputTime && _rb.velocity.y < 10)
             {
                 _rb.velocity += new Vector2(0, 8);
                 _rb.angularVelocity = (_rb.velocity.x + GameControl.Instance.WorldSpeed) * -10;
                 _doubleJump = false;
+                _jumpRequest = -10;
             }
 
         // rotate the trailing particles to match current player rotation
@@ -54,8 +75,10 @@ public class PlayerMovement : MonoBehaviour, IKillable
         velocity.x = new ParticleSystem.MinMaxCurve(worldDirection.x, worldDirection.x);
         velocity.y = new ParticleSystem.MinMaxCurve(worldDirection.y, worldDirection.y);
         velocity.z = new ParticleSystem.MinMaxCurve(0, 0);
+
         // change particle colors
         _particle.startColor = GameControl.LevelColor;
+        _groundedTime += Time.deltaTime;
     }
 
     private void OnCollisionStay2D(Collision2D collision) // using OnCollision for ground check, normally would check collision was at players "feet", but for this game its actually pretty fun to have any collision with the terrain work
@@ -63,6 +86,7 @@ public class PlayerMovement : MonoBehaviour, IKillable
         if (collision.gameObject.layer != 6)
         {
             _grounded = true;
+            _groundedTime = 0;
             _doubleJump = true;
             _rb.drag = 3;
         }
